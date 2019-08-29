36f3c9515c9d6e3c5528b7323c0f32002d29c4f1
mrwerner392 <mrwerner392@gmail.com>
2019-08-29 10:57:39 -0400

    passing local tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,19 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @owner.pets << @pet
+    elsif params.keys.include?("owner_id")
+      @owner = Owner.find(params["owner_id"])
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +27,22 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @owner.pets << @pet
+    elsif params.keys.include?("owner_id")
+      @owner = Owner.find(params["owner_id"])
+      @pet.update(owner_id: @owner.id)
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
